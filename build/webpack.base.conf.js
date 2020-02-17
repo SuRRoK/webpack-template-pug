f0f0ca22dd31affd8e0def026b7a61f448fd9557
surrok <surrok6@gmail.com>
2020-02-17 22:41:11 +0300

    Pug installed

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -8,6 +9,9 @@ const PATHS = {
     dist: path.join(__dirname, '../public'),
     assets: ('assets/'),
 };
+
+const PAGES_DIR = `${PATHS.src}/pug/pages`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
 
@@ -42,6 +46,9 @@ module.exports = {
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
+        }, {
+            test: /\.pug$/,
+            loader: 'pug-loader'
         }, {
             test: /\.css$/,
             use: [
@@ -88,10 +95,9 @@ module.exports = {
             {from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts`},
             {from: `${PATHS.src}/static`, to: ''},
         ]),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/index.html`,
-            filename: './index.html'
-        })
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/,'.html')}`
+        }))
     ]
 };
